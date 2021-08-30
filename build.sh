0fd952dec3d2f527dd66626f86e61ac54786dc75
Tien Le-Quang <quangtiencs@gmail.com>
2021-08-30 19:41:58 +0700

    Build with emcc

---
+++ build.sh
@@ -0,0 +1,1 @@
+emcc -O3 --bind -Icrfsuite/lib/cqdb/include -Icrfsuite/include -Iliblbfgs/include crfsuite/lib/crf/src/*.c crfsuite/swig/*.cpp crfsuite/lib/cqdb/src/cqdb.c crfsuite/lib/cqdb/src/lookup3.c liblbfgs/lib/*.c  main.cpp  -o wasm-crfsuite.js
